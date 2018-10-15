3be5e353f556360285f99963e7b299bb7e60aee5
Danny Brown <machinagod@googlemail.com>
2018-10-15 17:53:08 +0100

    updates to swagger config for season controller

--- Andgasm.BookieBreaker.SeasonParticipantSvc/Andgasm.BookieBreaker.SeasonParticipant.API/Controllers/SeasonsController.cs
+++ Andgasm.BookieBreaker.SeasonParticipantSvc/Andgasm.BookieBreaker.SeasonParticipant.API/Controllers/SeasonsController.cs
@@ -8,11 +8,15 @@ using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 using Andgasm.BookieBreaker.SeasonParticipant.API.Models;
 using System.Text;
+using System.Dynamic;
+using Newtonsoft.Json;
+using NSwag.Annotations;
 
 namespace Andgasm.BookieBreaker.SeasonParticipant.API.Controllers
 {
 
     [Route("api/[controller]")]
+    [ApiController]
     public class SeasonsController : Controller
     {
         IBusClient _newSeasonRegistrationBus;
@@ -24,10 +28,12 @@ namespace Andgasm.BookieBreaker.SeasonParticipant.API.Controllers
             _newSeasonRegistrationBus = seasregbusaccessor("NewSeason");
         }
 
-        [HttpGet]
-        public IEnumerable<SeasonResource> Get()
+        [HttpGet(Name = "GetAllSeasons")]
+        [SwaggerResponse(typeof(List<SeasonResource>), IsNullable = false,
+                         Description = "A collection of SeasonResources.")]
+        public async Task<IActionResult> GetAll()
         {
-            var d = _context.Seasons.Select(x => new SeasonResource()
+            var d = await  _context.Seasons.Select(x => new SeasonResource()
             {
                 Name = x.Name,
                 TournamentKey = x.TournamentKey,
@@ -38,12 +44,14 @@ namespace Andgasm.BookieBreaker.SeasonParticipant.API.Controllers
                 TournamentCode = x.TournamentCode,
                 SeasonCode = x.SeasonCode,
                 StageCode = x.StageCode
-            });
-            return d.ToList();
+            }).ToListAsync();
+            return Ok(d);
         }
 
-        [HttpPost]
-        public async Task<bool> StoreSeason([FromBody]SeasonResource model)
+        [HttpPost(Name = "CreateSeason")]
+        [SwaggerResponse(typeof(SeasonResource), IsNullable = false,
+                         Description = "The successfully created Season.")]
+        public async Task<IActionResult> Create([FromBody]SeasonResource model)
         {
             try
             {
@@ -65,25 +73,29 @@ namespace Andgasm.BookieBreaker.SeasonParticipant.API.Controllers
                         StageCode = model.StageCode
                     };
                     _context.Seasons.Add(season);
-                    await _newSeasonRegistrationBus.SendEvent(BuildNewSeasonEvent(model.TournamentCode, model.SeasonCode, model.StageCode, model.Key, model.RegionCode, model.CountryKey, model.StartDate, model.EndDate));
+                    await _newSeasonRegistrationBus.SendEvent(BuildNewSeasonEvent(model.TournamentCode, model.SeasonCode, model.StageCode, model.RegionCode, model.CountryKey));
                 }
+                else { Conflict($"The key '{model.Key}' already exists!"); }
                 if (dochange) await _context.SaveChangesAsync();
-                return dochange;
+                return Ok(model); 
             }
             catch (DbUpdateException pkex)
             {
                 // TODO: we are seeing this occaisionally due to async processing from multiple instances
                 //       its ok to swallow as we dont support data updates and if the key exists there is no need for dupe store
-                Console.WriteLine($"A primary key violation occured while saving player data: { pkex.Message }");
-                return false;
+                return Conflict($"A primary key violation occured while saving player data: { pkex.Message }");
             }
         }
 
-        private static BusEventBase BuildNewSeasonEvent(string tournamentcode, string seasoncode, string stagecode, string seasonkey, string regioncode, string countrykey, DateTime seasonstartdate, DateTime seasonenddate)
+        public static BusEventBase BuildNewSeasonEvent(string tournamentcode, string seasoncode, string stagecode, string regioncode, string countrycode)
         {
-            // TODO: temp to demo payload comms
-            string jsonpayload = string.Format(@"""tournamentcode"":""{0}"",""seasoncode"":""{1}"",""stagecode"":""{2}"",""seasonkey"":""{3}"",""regioncode"":""{4}"",""countrykey"":""{5}"",""startdate"":""{6}"",""enddate"":""{7}""", tournamentcode, seasoncode, stagecode, seasonkey, regioncode, countrykey, seasonstartdate, seasonenddate);
-            var payload = Encoding.UTF8.GetBytes("{" + jsonpayload + "}");
+            dynamic jsonpayload = new ExpandoObject();
+            jsonpayload.TournamentCode = tournamentcode;
+            jsonpayload.SeasonCode = seasoncode;
+            jsonpayload.StageCode = stagecode;
+            jsonpayload.RegionCode = regioncode;
+            jsonpayload.CountryCode = countrycode;
+            var payload = Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(jsonpayload));
             return new BusEventBase(payload);
         }
     }
