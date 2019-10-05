54b7c2cfaa1c3c10e9f71f34aafa1b0f04cff1cc
Danny Brown <machinagod@googlemail.com>
2019-10-05 23:42:28 +0100

    cicd test trigger

--- Andgasm.BB.SeasonParticipants/Andgasm.BB.SeasonParticipant.API/Startup/Startup.cs
+++ Andgasm.BB.SeasonParticipants/Andgasm.BB.SeasonParticipant.API/Startup/Startup.cs
@@ -11,7 +11,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 
 namespace Andgasm.BB.SeasonParticipant.API
-{
+{//
     public class Startup
     {
         public Startup(IConfiguration configuration)
